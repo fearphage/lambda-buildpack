a68f0a842d5fc35c51b4e4b685b3881aafd51865
Raul Reynoso <reynoso@Rauls-MacBook-Pro.local>
2018-01-18 14:09:44 -0600

    Change nvm download file remove obsolete files

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='LambdaDeploy',
-    version='0.1dev',
-    packages=['lambda-deploy',],
-    license='Proprietary',
-    long_description=open('README.txt').read(),
-)
