a18b0637e198fa94db19a43c7090a3b955efeba8
Phred <fearphage@gmail.com>
2018-06-19 20:56:18 -0400

    removed debug

--- src/deployer/cli.js
+++ src/deployer/cli.js
@@ -3,7 +3,7 @@ const program = require('commander');
 
 program
   .version('0.1.0')
-  // .option('-p, --path <absolute_path>', 'Absolute path to directory of app to deploy')
+  .option('-p, --path <absolute_path>', 'Absolute path to directory of app to deploy')
   .option('-r, --runtime <runtime>', 'AWS platform runtime: node6.10 or node4.3', /^nodejs(?:4\.3|6\.10|8\.10)$/, 'nodejs8.10')
   .option('-o, --orders_file <orders_file>', 'Path to the orders file for the app being deployed' )
   .option('-s, --service <service>', 'Name of the service being deployed' )
@@ -16,12 +16,10 @@ module.exports = argv => {
   const params = program.parse(argv);
 
   // Require user to specify directory to deploy
-  /*
   if (typeof params.path == 'undefined'){
       console.error('You must specify the directory where the applicaiton to deploy is located');
       process.exit(1)
   }
-  */
 
   if (params.runtime != 'nodejs6.10' && params.runtime != 'nodejs4.3' && params.runtime != 'nodejs8.10'){
       console.log('Supported runtimes values are nodejs8.10, nodejs6.10 and nodejs4.3. You entered \'' + params.runtime + '\'')
