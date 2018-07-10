d59a8fec8afcc1a31cd6e65cfd480fbaebadde2e
Phred <fearphage@gmail.com>
2018-07-10 15:19:35 -0500

    allow AWS_ROLE to be set from orders file - fixes #2

--- src/deployer/parse-orders-file.js
+++ src/deployer/parse-orders-file.js
@@ -4,11 +4,13 @@
  * except for the desired output
  *
  */
-const parseOrders = require('./orders');
 const program = require('./cli')(process.argv);
+const parsedOrders = require('./orders')(program.orders_file);
 
-console.error("### READ ORDER FILE")
-console.error("    ")
+const AWS_ROLE_ORDER_NAME = 'AWS_ROLE';
+
+console.error('### READ ORDER FILE');
+console.error('    ');
 
 // Stage has following format
 // $HQ_$SHIP_OrdersSha_ServiceSha_
@@ -17,15 +19,18 @@ console.error("    ")
 // second to last item - orders sha
 const [ordersSha, serviceSha] = program.container_name.split('-').splice(-2);
 
+const roleIndex = parsedOrders.findIndex(({ name }) => name === AWS_ROLE_ORDER_NAME);
+
 //   - setup template
 const data = {
-    stage: "dev",
-    bucket: `starphleet-lambda-deploys${process.env.AWS_ACCOUNT === 'glgdev' ? '' : '-glgapp'}`,
-    region: "us-east-1",
-    runtime: program.runtime,
-    environment: parseOrders(program.orders_file),
-    service_name: `${program.service}-${ordersSha}-${serviceSha}`,
-    aws_account: program.account
+  stage: 'dev',
+  bucket: `starphleet-lambda-deploys${process.env.AWS_ACCOUNT === 'glgdev' ? '' : '-glgapp'}`,
+  region: 'us-east-1',
+  runtime: program.runtime,
+  environment: parsedOrders,
+  service_name: `${program.service}-${ordersSha}-${serviceSha}`,
+  aws_account: program.account,
+  aws_role: roleIndex > -1 ? parsedOrders[roleIndex].value : 'lambda-deploy-function-role',
 };
 
 // just for debug purposes
